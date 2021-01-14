7f442e65a5b10ceccbd2c2951a1e4c1ae3d18c70
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-01-14 13:15:13 -0300

    Test against pulp-ansible@master
    No-Issue

--- .travis/pre_before_install.sh
+++ .travis/pre_before_install.sh
@@ -4,3 +4,7 @@ if [[ "$TRAVIS_PULL_REQUEST" != 'false' ]]; then
   pip install requests
   python .travis/custom_check_pull_request.py
 fi
+
+# Unpin pulpcore and pulp-ansible
+sed -i '0,/pulpcore/{s/pulpcore.*/pulpcore",/}' setup.py
+sed -i '0,/pulp-ansible/{s/pulp-ansible.*/pulp-ansible",/}' setup.py
